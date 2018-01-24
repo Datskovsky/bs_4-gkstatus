ec70a84e4a99af5e42178374dbb99dbdf0b08102
Максим <30493170+Datskovsky@users.noreply.github.com>
2018-01-24 17:24:57 +0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import 'bootstrap';
+
+// bootstrap carousel settings
+$ ('.carousel').carousel({
+    interval: false
+})
+
+// owl carousel
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+import $ from 'jquery';
+import 'owl.carousel';
+
+// lightbox2
+import 'lightbox2/src/js/lightbox.js';
+
+// global import file
+import './owl.js';
